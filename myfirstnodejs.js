772ab4d7a55f1c4e74c973e622c7471a58e025b4
Alex <alex.isayenko@gmail.com>
2019-03-23 09:16:51 +0200

    listen to port 80

---
+++ myfirstnodejs.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var url = require('url')
+
+// http://localhost:8080/get-sum?a=11&b=120
+function listener(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  var urlParts = url.parse(req.url);
+
+  if (urlParts.pathname == '/get-sum'){
+
+    var q = url.parse(req.url, true).query;
+    res.write(q.a + " + " + q.b + " = " + sum(q.a, q.b));
+  }
+  
+  res.end();
+}
+
+http.createServer(listener).listen(80);
+
+function sum(a, b){
+  return parseInt(a) + parseInt(b);
+}
